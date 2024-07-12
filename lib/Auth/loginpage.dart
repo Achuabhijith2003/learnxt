7c3f501c403f26351a976c203572a34a65222d9a
jrdeveloper2003 <jrdeveloper2003@gmail.com>
2024-07-12 16:28:28 +0000

    splash created

--- lib/Auth/loginpage.dart
+++ lib/Auth/loginpage.dart
@@ -11,7 +11,73 @@ class _LoginpageState extends State<Loginpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Firebase Init"),),
+      // appBar: AppBar(
+      //   title: const Center(child: Text("LearnXT")),
+      // ),
+      body: SafeArea(
+        child: Container(
+          //  height: 250,
+        // width: double.maxFinite,
+        //  margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: const  DecorationImage(
+            image:  AssetImage("assets/download (2).jpeg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 100),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  // Image.asset("assets/backgrondimage.jpg"),
+                  const Text(
+                    "Welcome Back",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green),
+                  ),
+                  //  const   Text("EmailID"),
+                const  Padding(
+                    padding:  EdgeInsets.only(top: 10, left: 20, right: 20),
+                    child:  TextField(
+                      decoration: InputDecoration(
+                          hintText: "Enter your EmailID",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(20)))),
+                      keyboardType: TextInputType.emailAddress,
+                      obscureText: true,
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  // const  Text("Password"),
+                  const Padding(
+                     padding:  EdgeInsets.only(top: 10, left: 20, right: 20),
+                     child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Enter your Password",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(20)))),
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText: true,
+                      style: TextStyle(fontSize: 20),
+                                 ),
+                   ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(onPressed: () {}, child: const Text("Login")),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
+
+  login() {}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learnxt/Auth/loginpage.dart';
+import 'package:learnxt/Splash/splash.dart';
 
 void main(List<String> args) {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,7 +14,7 @@ class Learnxt extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home : Loginpage()
+      home : Splash()
     );
   }
 }
