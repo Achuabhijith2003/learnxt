e084a2bb7ef16b965e3998e5596ad95b4f5fdaa7
Abhijith JR <Achuabhijithjr@outlook.com>
2024-08-16 20:46:29 +0530

    Convert pdf text into embeded doned

--- lib/Services/data_embedded.dart
+++ lib/Services/data_embedded.dart
@@ -5,13 +5,18 @@ import 'package:learnxt/consts.dart';
 import 'package:pdf_text/pdf_text.dart';
 
 class DataEmbedded {
-  // ignore: non_constant_identifier_names
-  Generate_dataEmbedded(String text) async {
-    final model =
-        GenerativeModel(model: 'text-embedding-004', apiKey: GEMINI_API_KEY);
-    final content = Content.text(text);
-    final result = await model.embedContent(content);
-    print(result.embedding.values);
+  Future<List<List<double>>> Generate_dataEmbedded(
+      List<String> textList) async {
+    final embeddings = <List<double>>[];
+    for (final text in textList) {
+      final model =
+          GenerativeModel(model: 'text-embedding-004', apiKey: GEMINI_API_KEY);
+      final content = Content.text(text);
+      final result = await model.embedContent(content);
+      embeddings.add(result.embedding.values);
+    }
+    print(embeddings);
+    return embeddings;
   }
 
   // ignore: non_constant_identifier_names
@@ -24,10 +29,17 @@ class DataEmbedded {
   }
 
   //pdf to text
-  Future<String> pdfextract(File pdfFile) async {
+  // Future<String> pdfextract(File pdfFile) async {
+  //   final pdfDoc = await PDFDoc.fromFile(pdfFile);
+  //   List<String> text = (await pdfDoc.text) as List<String>;
+  //   Generate_dataEmbedded(text);
+  //   return "null";
+  // }
+
+  Future<List<List<double>>> pdfextract(File pdfFile) async {
     final pdfDoc = await PDFDoc.fromFile(pdfFile);
     final text = await pdfDoc.text;
-    print(text);
-    return text;
+    final textList = text.split('\n'); // Split text into a list of strings
+    return Generate_dataEmbedded(textList);
   }
 }
